3c29bd711037619f9d484676875fee5fe32bd7fb
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-06 21:47:42 -0400

    Update rackup

--- faye.ru
+++ faye.ru
@@ -1,4 +1,11 @@
 require 'faye'
 Faye::WebSocket.load_adapter('thin')
 faye_server = Faye::RackAdapter.new(:mount => '/faye', :timeout => 45)
+faye_server.add_extension(Debugger)
 run faye_server
+
+class Debugger
+  def incoming(msg)
+    puts msg.inspect
+  end
+end
--- test_faye.rb
+++ test_faye.rb
@@ -3,8 +3,8 @@ require 'eventmachine'
 
   EM.run {
     client = Faye::Client.new('http://fallinggarden.com:9000/faye')
-    client.subscribe('/rooms/fake') do |message|
+    client.subscribe('/1') do |message|
       puts message.inspect
     end
-    client.publish('/rooms/fake', 'text' => 'It works!')
+    client.publish('/1', 'text' => 'It works!')
   }
