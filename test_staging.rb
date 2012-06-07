f0230f3bdd8253e0ab35eb650cb8f06d6d58020a
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-07 16:05:49 -0400

    Split test script

---
+++ test_staging.rb
@@ -0,0 +1,10 @@
+require 'faye'
+require 'eventmachine'
+
+  EM.run {
+    client = Faye::Client.new('http://fallinggarden.com:9000/faye')
+    client.subscribe('/room') do |message|
+      puts message.inspect
+    end
+    client.publish('/room', message: {'text' => 'It works (for reals)!'})
+  }
