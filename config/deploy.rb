fc9a7b7a0c09ad26703328f9f85ffffa7ba2b7a5
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-11 16:58:10 -0400

    Finish config for deploying with god

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,28 @@
-MODE = "production"
+MODE = ENV["DEPLOY_MODE"] || "production"
+
 PRODUCTION_SERVER = "fallingfoundry.com"
 STAGING_SERVER = "fallinggarden.com"
+APPLICATION_NAME = "FayeServer"
+SCRIPT_NAME = "faye"
+REPOSITORY = "git://github.com/HungryAcademyTeam4/FayeServer.git"
 SERVER = MODE == "production" ? PRODUCTION_SERVER : STAGING_SERVER
-
+START_COMMAND = 'rainbows /apps/FayeServer/current/faye.ru -c /apps/FayeServer/current/rainbows.conf -E production -p 9000'
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-# require 'bundler/capistrano'
- require 'rvm/capistrano'
+require 'rvm/capistrano'
 server SERVER, :web, :db, :app, primary: true
 set :user, "root"
-set :application, "FayeServer"
-
-#set :bundle_dir, "/apps/#{application}/current"
-#set :bundle_flags, "--deployment --quiet --system"
+set :application, APPLICATION_NAME
 
 set :deploy_to, "/apps/#{application}"
 set :deploy_via, :remote_cache
 
 set :scm, "git"
-set :repository,  "git://github.com/HungryAcademyTeam4/FayeServer.git"
+set :repository, REPOSITORY
 set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
-
 
 namespace :deploy do
   task :mkdirs do
@@ -32,29 +30,34 @@ namespace :deploy do
     run "mkdir /apps/god_scripts -p"
   end
   task :create_god_script do
-    run 'cd /apps/god_scripts && touch faye.rb && rm faye.rb && touch faye.rb'
-    run 'echo God.watch do \|w\| >> /apps/god_scripts/faye.rb'
-    run 'echo w.log = \"/faye.log\" >> /apps/god_scripts/faye.rb'
-    run 'echo w.name = \"faye\" >> /apps/god_scripts/faye.rb'
-    run 'echo w.start = \"rainbows /apps/FayeServer/current/faye.ru -c /apps/FayeServer/current/rainbows.conf -E production -p 9000\"  >> /apps/god_scripts/faye.rb'
-    run 'echo w.keepalive >> /apps/god_scripts/faye.rb'
-    run 'echo end >> /apps/god_scripts/faye.rb'
+    run %^cd /apps/god_scripts && touch #{SCRIPT_NAME}.rb && rm #{SCRIPT_NAME}.rb && touch #{SCRIPT_NAME}.rb^
+    run %^echo God.watch do \\\|w\\\| >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
+    run %^echo w.log = \\\"/#{SCRIPT_NAME}.log\\\" >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
+    run %^echo w.name = \\\"#{SCRIPT_NAME}\\\" >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
+    run %^echo w.start = \\\"#{START_COMMAND}\\\" >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
+    run %^echo w.keepalive >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
+    run %^echo end >> /apps/god_scripts/#{SCRIPT_NAME}.rb^
   end
   task :bundle do
     run "cd /apps/#{application}/current && bundle install --system"
   end
   task :start do
     run "cd / && god"
-    run "cd / && god load apps/god_scripts/faye.rb"
-    run "cd / && god start faye"
+    run "cd / && god load apps/god_scripts/#{SCRIPT_NAME}.rb"
+    run "cd / && god start #{SCRIPT_NAME}"
   end
   task :stop do
-    run "cd / && god stop faye"
+    run "cd / && god stop #{SCRIPT_NAME}"
   end
   task :restart do
     stop
     start
   end
+  task :ensure_god_running do
+    run "cd / && god"
+  end
+  before "deploy:start", "deploy:ensure_god_running"
+  before "deploy:stop", "deploy:ensure_god_running"
   before "deploy", "deploy:mkdirs"
   before "deploy", "deploy:create_god_script"
   after "deploy", "deploy:bundle"
